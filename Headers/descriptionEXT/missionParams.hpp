d691a1d1141deb104b5820e2c716a734551fe607
Ansible2 <55198007+Ansible2@users.noreply.github.com>
2020-08-12 19:58:10 -0600

    Update missionParams.hpp

---
+++ Headers/descriptionEXT/missionParams.hpp
@@ -0,0 +1,29 @@
+class enemyUnits
+{
+	title = "Use Enemies From";
+	texts[] = {"Vanilla OPTRE","OPCAN 3.0","JTFS Armory"};
+	values[] = {0,1,2};
+	default = 0;
+};
+class enemyVehicles
+{
+	title = "Use Enemy Vehicles From";
+	texts[] = {"Vanilla OPTRE","Fire Team Zulu","JTFS Armory"};
+	values[] = {0,1,2};
+	default = 0;
+};
+
+class friendlyUnits
+{
+	title = "Use Firendlies From";
+	texts[] = {"Vanilla OPTRE","OPCAN 3.0","Fire Team Zulu"};
+	values[] = {0,1,2};
+	default = 0;
+};
+class friendlyVehicles
+{
+	title = "Use Firendly Vehicles From";
+	texts[] = {"Vanilla OPTRE","OPCAN 3.0","Fire Team Zulu"};
+	values[] = {0,1,2};
+	default = 0;
+};
