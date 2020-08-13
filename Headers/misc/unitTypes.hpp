b35f5b32e65e02cff32c551ac4bc9d2f1828f3bb
Ansible2 <55198007+Ansible2@users.noreply.github.com>
2020-08-12 19:58:02 -0600

    Create unitTypes.hpp

---
+++ Headers/misc/unitTypes.hpp
@@ -0,0 +1,83 @@
+// Vanilla
+OPF_manTypesURF_vanilla = [
+	"OPTRE_Ins_URF_AA_Specialist",
+	"OPTRE_Ins_URF_Assist_Autorifleman",
+	"OPTRE_Ins_URF_Autorifleman",
+	"OPTRE_Ins_URF_Marksman",
+	"OPTRE_Ins_URF_Engineer",
+	"OPTRE_Ins_URF_Grenadier",
+	"OPTRE_Ins_URF_Radioman",
+	"OPTRE_Ins_URF_Rifleman_AT",
+	"OPTRE_Ins_URF_Rifleman_AR",
+	"OPTRE_Ins_URF_Rifleman_BR",
+	"OPTRE_Ins_URF_Rifleman_Light",
+	"OPTRE_Ins_URF_Medic",
+	"OPTRE_Ins_URF_TeamLead",
+	"OPTRE_Ins_URF_Marksman",
+	"OPTRE_Ins_URF_SquadLead"	
+];
+OPF_largeTrucksURF_vanilla = [
+	"O_Truck_03_ammo_F",
+	"O_Truck_03_fuel_F",
+	"O_Truck_03_medical_F",
+	"O_Truck_03_repair_F",
+	"O_Truck_03_transport_F",
+	"O_Truck_03_covered_F"
+];
+OPF_miscWarthogsURF_vanilla = [
+	"OPTRE_M12_FAV_ins",
+	"OPTRE_M914_RV_ins",
+	"OPTRE_M12R_AA_ins"
+];
+
+
+// Opcan
+OPF_manTypesURF_OPCAN = [
+	"LM_OPCAN_URA_AutoRifleman",
+	"LM_OPCAN_URA_Breacher",
+	"LM_OPCAN_URA_Medic",
+	"LM_OPCAN_URA_Engineer",
+	"LM_OPCAN_URA_GL",
+	"LM_OPCAN_URA_Marksman",
+	"LM_OPCAN_URA_Rifleman",
+	"LM_OPCAN_URA_Rifleman_AT",
+	"LM_OPCAN_URA_RTO",
+	"LM_OPCAN_URA_SL",
+	"LM_OPCAN_URA_TL"
+];
+
+
+
+// JTFS
+OPF_manTypesURF_JTFS = [
+	"JTFS_Autorifleman",
+	"JTFS_Grenadier",
+	"JTFS_Marksman",
+	"JTFS_Medic",
+	"JTFS_Rifleman_AT",
+	"JTFS_Rifleman_MA5A"
+];
+OPF_largeTrucksURF_JTFS = [
+	"JTFS_HDV134_Resupply",
+	"JTFS_HDV134_TT_C",
+	"JTFS_HDV134_Maintenance",
+	"JTFS_HDV134_TT",
+	"JTFS_HDV134_Tanker_C"
+];
+OPF_miscWarthogsURF_JTFS = [
+	"JTFS_M914_RV",
+	"JTFS_M12R_AA",
+	"JTFS_M12G1_LRV",
+	"JTFS_M813_TT",
+	"JTFS_M12_FAV"
+];
+
+
+
+// Fire Team Zulu
+OPF_miscWarthogsURF_zulu = [
+	"VES_M12_URF",
+	"VES_M813_TT_URF",
+	"OPTRE_M12R_AA_ins",
+	"OPTRE_M914_RV_ins"
+];
